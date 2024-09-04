7aabdb477346d74f51209bf68a92fd68366715b4
HouYong Yang <240011814@qq.com>
2024-09-04 11:53:41 +0800

    1

--- Djava/build.sh
+++ Djava/build.sh
@@ -2,7 +2,7 @@
 
 current_datetime=$(date +%Y%m%d_%H%M%S)
 git_c=$(git rev-parse --short HEAD)
-app_name='demo'
+version_name="${current_datetime}_${git_c}"
 image_name="djava:latest"
 echo  ${image_name}
-docker build --build-arg GIT_COMMIT=${image_name} -t ${image_name} .
+docker build --build-arg GIT_COMMIT=${version_name} -t ${image_name} .
