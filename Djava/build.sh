a37871ab22919c3385a003f7f0cd4703d8fdaf9e
HouYong Yang <240011814@qq.com>
2024-09-04 08:59:50 +0800

    1

--- Djava/build.sh
+++ Djava/build.sh
@@ -3,6 +3,6 @@
 current_datetime=$(date +%Y%m%d_%H%M%S)
 git_c=$(git rev-parse --short HEAD)
 app_name='demo'
-image_name="${app_name}_${current_datetime}_${git_c}"
+image_name="${app_name}:${current_datetime}_${git_c}"
 echo  ${image_name}
 docker build -t ${image_name}
