54b6db2893f889264a93feb769c9dfcd9c081cd3
HouYong Yang <240011814@qq.com>
2024-09-04 10:50:16 +0800

    1

---
+++ Djava/src/main/java/com/yhy/djava/controller/HealthCheckController.java
@@ -0,0 +1,29 @@
+package com.yhy.djava.controller;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @Author： HouYong Yang
+ * @Date： 2024/9/4 9:36
+ * @Describe：
+ */
+@RestController
+public class HealthCheckController {
+
+    private final Environment env;
+
+    public HealthCheckController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping(value = "ping")
+    public Map check(){
+        return  Map.of("version", Optional.ofNullable(env.getProperty("codeVersion")).orElse(""));
+    }
+}
